7e587eab17589630bf8babc04404bab3033324fb
plastilinko <33184157+plastilinko@users.noreply.github.com>
2017-11-09 02:46:02 +0300

    update

--- src/test/java/ru/fixtest/fixtest.java
+++ src/test/java/ru/fixtest/fixtest.java
@@ -4,11 +4,6 @@ package ru.fixtest;
         import com.codeborne.selenide.Selenide;
         import org.junit.Test;
         import org.openqa.selenium.By;
-        import org.openqa.selenium.WebDriver;
-        import org.openqa.selenium.chrome.ChromeDriver;
-        import com.codeborne.selenide.WebDriverRunner;
-        import org.openqa.selenium.support.ui.WebDriverWait;
-
         import static com.codeborne.selenide.Condition.*;
         import static com.codeborne.selenide.Configuration.browser;
         import static com.codeborne.selenide.Selectors.*;
@@ -23,13 +18,8 @@ public class fixtest {
 
     @Test
     public void test01() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\.m2\\repository\\webdriver\\chromedriver\\win32\\2.33\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        WebDriverRunner.setWebDriver(driver);
-
-        browser = "driver";
+        browser = "chrome";
         Configuration.timeout = 10000;
-
         open("http://rulkov.ru/qa/");
         $(byText("Операция сложения")).shouldBe(visible).click();
         $(byText("Login")).shouldBe(visible);
@@ -46,18 +36,12 @@ public class fixtest {
         $(withText("Результат")).shouldBe(visible);
         $(withText("Итого")).shouldBe(visible);
         $(screenshot(Thread.currentThread().getStackTrace()[1].getMethodName()));
-        driver.close();
-        driver.quit();
+        Selenide.close();
     }
     @Test
     public void test21() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\.m2\\repository\\webdriver\\chromedriver\\win32\\2.33\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        WebDriverRunner.setWebDriver(driver);
-
-        browser = "driver";
+        browser = "chrome";
         Configuration.timeout = 10000;
-
         open("http://rulkov.ru/qa/");
         $(byText("Операция сложения")).shouldBe(visible).click();
         $(byText("Login")).shouldBe(visible);
@@ -66,107 +50,71 @@ public class fixtest {
         $(By.className("btn-success")).shouldBe(enabled).pressEnter();
         $(withText(String.valueOf(truelogin))).shouldBe(visible);
         $(screenshot(Thread.currentThread().getStackTrace()[1].getMethodName()));
-        driver.close();
-        driver.quit();
+        Selenide.close();
     }
     @Test
     public void test22() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\.m2\\repository\\webdriver\\chromedriver\\win32\\2.33\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        WebDriverRunner.setWebDriver(driver);
-
-        browser = "driver";
+        browser = "chrome";
         Configuration.timeout = 10000;
-
         open("http://rulkov.ru/qa/");
         $(byText("Операция сложения")).shouldBe(visible).click();
         $(byText("Login")).shouldBe(visible);
         $(By.name("password")).val(String.valueOf(truepassword));
         $(By.className("btn-success")).shouldBe(disabled);
         $(screenshot(Thread.currentThread().getStackTrace()[1].getMethodName()));
-        driver.close();
-        driver.quit();
+        Selenide.close();
     }
     @Test
     public void test23() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\.m2\\repository\\webdriver\\chromedriver\\win32\\2.33\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        WebDriverRunner.setWebDriver(driver);
-
-        browser = "driver";
+        browser = "chrome";
         Configuration.timeout = 10000;
-
         open("http://rulkov.ru/qa/");
         $(byText("Операция сложения")).shouldBe(visible).click();
         $(byText("Login")).shouldBe(visible);
         $(By.name("password")).val("somerandompassword");
         $(By.className("btn-success")).shouldBe(disabled);
         $(screenshot(Thread.currentThread().getStackTrace()[1].getMethodName()));
-        driver.close();
-        driver.quit();
+        Selenide.close();
     }
     @Test
     public void test24() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\.m2\\repository\\webdriver\\chromedriver\\win32\\2.33\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        WebDriverRunner.setWebDriver(driver);
-
-        browser = "driver";
+        browser = "chrome";
         Configuration.timeout = 10000;
-
         open("http://rulkov.ru/qa/");
         $(byText("Операция сложения")).shouldBe(visible).click();
         $(byText("Login")).shouldBe(visible);
         $(By.className("btn-success")).shouldBe(disabled);
         $(screenshot(Thread.currentThread().getStackTrace()[1].getMethodName()));
-        driver.close();
-        driver.quit();
+        Selenide.close();
     }
     @Test
     public void test25() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\.m2\\repository\\webdriver\\chromedriver\\win32\\2.33\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        WebDriverRunner.setWebDriver(driver);
-
-        browser = "driver";
+        browser = "chrome";
         Configuration.timeout = 10000;
-
         open("http://rulkov.ru/qa/");
         $(byText("Операция сложения")).shouldBe(visible).click();
         $(byText("Login")).shouldBe(visible);
         $(By.name("username")).val(String.valueOf(truelogin));
         $(By.className("btn-success")).shouldBe(disabled);
         $(screenshot(Thread.currentThread().getStackTrace()[1].getMethodName()));
-        driver.close();
-        driver.quit();
+        Selenide.close();
     }
     @Test
     public void test26() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\.m2\\repository\\webdriver\\chromedriver\\win32\\2.33\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        WebDriverRunner.setWebDriver(driver);
-
-        browser = "driver";
+        browser = "chrome";
         Configuration.timeout = 10000;
-
         open("http://rulkov.ru/qa/");
         $(byText("Операция сложения")).shouldBe(visible).click();
         $(byText("Login")).shouldBe(visible);
         $(By.name("username")).val("somerandomname");
         $(By.className("btn-success")).shouldBe(disabled);
         $(screenshot(Thread.currentThread().getStackTrace()[1].getMethodName()));
-        driver.close();
-        driver.quit();
+        Selenide.close();
     }
     @Test
     public void test27() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\.m2\\repository\\webdriver\\chromedriver\\win32\\2.33\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        WebDriverRunner.setWebDriver(driver);
-
-        browser = "driver";
+        browser = "chrome";
         Configuration.timeout = 10000;
-
         open("http://rulkov.ru/qa/");
         $(byText("Операция сложения")).shouldBe(visible).click();
         $(byText("Login")).shouldBe(visible);
@@ -176,18 +124,12 @@ public class fixtest {
             $(By.className("btn-success")).shouldBe(disabled);
             $(screenshot(Thread.currentThread().getStackTrace()[1].getMethodName()));
         }finally{
-        driver.close();
-        driver.quit();
+        Selenide.close();
     }}
     @Test
     public void test31() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\.m2\\repository\\webdriver\\chromedriver\\win32\\2.33\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        WebDriverRunner.setWebDriver(driver);
-
-        browser = "driver";
+        browser = "chrome";
         Configuration.timeout = 10000;
-
         open("http://rulkov.ru/qa/");
         $(byText("Операция сложения")).shouldBe(visible).click();
         $(byText("Login")).shouldBe(visible);
@@ -201,16 +143,11 @@ public class fixtest {
         $(By.name("value2")).val("1");
         $(byValue("Вперед")).shouldBe(disabled);
         $(screenshot(Thread.currentThread().getStackTrace()[1].getMethodName()));
-        driver.close();
-        driver.quit();
+        Selenide.close();
     }
     @Test
     public void test32() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\.m2\\repository\\webdriver\\chromedriver\\win32\\2.33\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        WebDriverRunner.setWebDriver(driver);
-
-        browser = "driver";
+        browser = "chrome";
         Configuration.timeout = 10000;
 
         String value1 = "11";
@@ -233,18 +170,12 @@ public class fixtest {
         $(By.tagName("table")).shouldHave(text(String.valueOf(value2)));
         $(By.tagName("table")).shouldHave(text(String.valueOf(value3)));
         $(screenshot(Thread.currentThread().getStackTrace()[1].getMethodName()));
-        driver.close();
-        driver.quit();
+        Selenide.close();
     }
     @Test
     public void test33() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\.m2\\repository\\webdriver\\chromedriver\\win32\\2.33\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        WebDriverRunner.setWebDriver(driver);
-
-        browser = "driver";
+        browser = "chrome";
         Configuration.timeout = 10000;
-
         open("http://rulkov.ru/qa/");
         $(byText("Операция сложения")).shouldBe(visible).click();
         $(byText("Login")).shouldBe(visible);
@@ -261,18 +192,12 @@ public class fixtest {
             $(byValue("Назад")).shouldBe(enabled);
             $(screenshot(Thread.currentThread().getStackTrace()[1].getMethodName()));
         }finally{
-        driver.close();
-        driver.quit();
+        Selenide.close();
     }}
     @Test
     public void test34() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\.m2\\repository\\webdriver\\chromedriver\\win32\\2.33\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        WebDriverRunner.setWebDriver(driver);
-
-        browser = "driver";
+        browser = "chrome";
         Configuration.timeout = 10000;
-
         open("http://rulkov.ru/qa/");
         $(byText("Операция сложения")).shouldBe(visible).click();
         $(byText("Login")).shouldBe(visible);
@@ -289,18 +214,12 @@ public class fixtest {
         $(withText("Результат")).shouldBe(visible);
         $(withText("Итого")).shouldBe(visible);
         $(screenshot(Thread.currentThread().getStackTrace()[1].getMethodName()));
-        driver.close();
-        driver.quit();
+        Selenide.close();
     }
     @Test
     public void test41() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\.m2\\repository\\webdriver\\chromedriver\\win32\\2.33\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        WebDriverRunner.setWebDriver(driver);
-
-        browser = "driver";
+        browser = "chrome";
         Configuration.timeout = 10000;
-
         open("http://rulkov.ru/qa/");
         $(byText("Операция сложения")).shouldBe(visible).click();
         $(byText("Login")).shouldBe(visible);
@@ -312,18 +231,12 @@ public class fixtest {
         $(byText("Операция сложения")).shouldBe(visible).click();
         $(byText("Login")).shouldBe(visible);
         $(screenshot(Thread.currentThread().getStackTrace()[1].getMethodName()));
-        driver.close();
-        driver.quit();
+        Selenide.close();
     }
     @Test
     public void test42() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\.m2\\repository\\webdriver\\chromedriver\\win32\\2.33\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        WebDriverRunner.setWebDriver(driver);
-
-        browser = "driver";
+        browser = "chrome";
         Configuration.timeout = 10000;
-
         open("http://rulkov.ru/qa/");
         $(byText("Операция сложения")).shouldBe(visible).click();
         $(byText("Login")).shouldBe(visible);
@@ -338,23 +251,15 @@ public class fixtest {
         $(withText("Подтверждение данных")).shouldBe(visible);
         $(byValue("Вперед")).shouldBe(enabled).pressEnter();
         try {
-            WebDriverWait wait = new WebDriverWait(driver, 5001);
-/*        $(withText("Итого")).waitUntil((visible),5001);*/
-            $(screenshot(Thread.currentThread().getStackTrace()[1].getMethodName()));
-/*        Selenide.close();*/
+        $(withText("Итого")).waitUntil((visible),5001);
+        $(screenshot(Thread.currentThread().getStackTrace()[1].getMethodName()));
         }finally{
-        driver.close();
-        driver.quit();
+        Selenide.close();
     }}
     @Test
     public void test43() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\.m2\\repository\\webdriver\\chromedriver\\win32\\2.33\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        WebDriverRunner.setWebDriver(driver);
-
-        browser = "driver";
+        browser = "chrome";
         Configuration.timeout = 10000;
-
         open("http://rulkov.ru/qa/");
         $(byText("Операция сложения")).shouldBe(visible).click();
         $(byText("Login")).shouldBe(visible);
@@ -367,18 +272,12 @@ public class fixtest {
             $(withText(String.valueOf(truelogin))).shouldBe(visible);
             $(screenshot(Thread.currentThread().getStackTrace()[1].getMethodName()));
         }finally{
-        driver.close();
-        driver.quit();
+        Selenide.close();
     }}
     @Test
     public void test44() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\.m2\\repository\\webdriver\\chromedriver\\win32\\2.33\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        WebDriverRunner.setWebDriver(driver);
-
-        browser = "driver";
+        browser = "chrome";
         Configuration.timeout = 10000;
-
         open("http://rulkov.ru/qa/");
         $(byText("Операция сложения")).shouldBe(visible).click();
         $(byText("Login")).shouldBe(visible);
@@ -397,7 +296,6 @@ public class fixtest {
             $(withText("Итого")).shouldBe(text("12345678901234567892"));
             $(screenshot(Thread.currentThread().getStackTrace()[1].getMethodName()));
         }finally{
-        driver.close();
-        driver.quit();
+        Selenide.close();
     }}
 }
